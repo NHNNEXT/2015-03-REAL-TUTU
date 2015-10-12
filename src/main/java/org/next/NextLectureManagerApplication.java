cc035ffc3161c99bb1cb6157d51ad7bf72074073
ho <hataeho1@naver.com>
2015-10-12 17:03:12 +0900

    초기 서버 환경 설정

---
+++ src/main/java/org/next/NextLectureManagerApplication.java
@@ -0,0 +1,12 @@
+package org.next;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextLectureManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextLectureManagerApplication.class, args);
+    }
+}
