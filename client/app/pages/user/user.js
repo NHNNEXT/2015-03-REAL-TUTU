97f43bc60e4f26e450d2dc950ca2534fc1ff43aa
dev <parksungho86@gmail.com>
2015-10-24 18:07:51 +0900

    re

--- client/app/pages/user/user.js
+++
@@ -1,9 +0,0 @@
-angular.module('clientApp').config(function ($stateProvider) {
-  $stateProvider
-    .state('user', {
-      url: "/user/:id",
-      templateUrl: "/pages/user/user.html",
-      controller: function () {
-      }
-    });
-});
