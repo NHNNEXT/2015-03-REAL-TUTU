70624713c7e167277ff0e353a62ad7691572b824
ho <hataeho1@naver.com>
2015-10-28 20:06:32 +0900

    remove service, controller, repository

--- src/main/java/org/next/lecturemanager/repository/SubjectRepository.java
+++
@@ -1,6 +0,0 @@
-package org.next.lecturemanager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<Subject, Long> {
-}
