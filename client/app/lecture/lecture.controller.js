16f89cde55acf4adca3d0a95655a518c6fc80932
dev <parksungho86@gmail.com>
2015-11-01 18:31:35 +0900

    Dtos And Responses

--- client/app/lecture/lecture.controller.js
+++ client/app/lecture/lecture.controller.js
@@ -1,55 +1,56 @@
-(function() {
+(function () {
   'use strict';
 
   angular
     .module('clientApp')
-    .controller('LectureController',LectureController);
+    .controller('LectureController', LectureController);
 
-    /* @ngInject */
-    function LectureController($scope,$log,lecture,modal) {
-      var vm = $scope;
-      vm.createLecture = createLecture;
-      _init();
+  /* @ngInject */
+  function LectureController($scope, $log, lecture, modal) {
+    var vm = $scope;
+    vm.createLecture = createLecture;
+    _init();
 
-      function _init() {
-        // change it to $watch
-        //_lectures(true);
-        //_lectures(false);
-      }
+    function _init() {
+      // change it to $watch
+      //_lectures(true);
+      //_lectures(false);
+    }
 
-      function _lectures(isMyLecture, params) {
-        lecture
-          .getLectures(isMyLecture, params)
-          .then(function (response) {
-            $log.info('lecture list: ' , response.data);
-            if(isMyLecture) {
-              vm.myLectures = response.data;
-            } else {
-              vm.otherLectures = response.data;
-            }
-          });
-      }
+    function _lectures(isMyLecture, params) {
+      lecture
+        .getLectures(isMyLecture, params)
+        .then(function (response) {
+          $log.info('lecture list: ', response.data);
+          if (isMyLecture) {
+            vm.myLectures = response.data;
+          } else {
+            vm.otherLectures = response.data;
+          }
+        });
+    }
 
-      function createLecture() {
-        modal
+    function createLecture() {
+      modal
         .open('lg', 'create-lecture.html', 'CreateLectureController')
-        .then(function(result) {
+        .then(function (result) {
           $log.info('create lecture result: ', result);
           vm.myLectures.unshift(result);
-        },function(error) {});
-      }
+        }, function (error) {
+        });
+    }
 
 
     $scope.$watch(function () {
       return vm.myLectureName;
-    }, function(newVal,oldVal) {
-      _lectures(true, {name:vm.myLectureName});
+    }, function (newVal, oldVal) {
+      _lectures(true, {name: vm.myLectureName});
     });
 
-    $scope.$watch(function() {
+    $scope.$watch(function () {
       return vm.otherLectureName;
-    }, function(newVal, oldVal) {
-      _lectures(false, {name:vm.otherLectureName});
+    }, function (newVal, oldVal) {
+      _lectures(false, {name: vm.otherLectureName});
     });
   }
 
