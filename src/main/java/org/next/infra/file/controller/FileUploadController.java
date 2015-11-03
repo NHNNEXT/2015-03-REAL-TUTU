fb48b2e2ac12f44f00d6da5cd87cc65627f95795
ho <hataeho1@naver.com>
2015-11-04 00:34:26 +0900

    file upload

--- src/main/java/org/next/infra/file/controller/FileUploadController.java
+++ src/main/java/org/next/infra/file/controller/FileUploadController.java
@@ -2,48 +2,42 @@ package org.next.infra.file.controller;
 
 import org.next.infra.common.dto.CommonJsonResponse;
 import org.next.infra.reponse.ResponseCode;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.UUID;
 
 import static org.next.infra.common.dto.CommonJsonResponse.successJsonResponse;
 
 @RestController
 @RequestMapping("/api/v1/upload")
+@PropertySource("classpath:config.properties")
 public class FileUploadController {
 
-    private static final String FILE_STORAGE_DIRECTORY = "./../../../client/app/uploads/";
-
-    private static String SERVLET_CONTEXT_ABSOLUTE_PATH;
-    private static String FILE_SAVE_DIRECTORY_PATH;
-
-    @Autowired
-    private ServletContext servletContext;
-
-    @PostConstruct
-    public void postConstruct() {
-        SERVLET_CONTEXT_ABSOLUTE_PATH = servletContext.getRealPath("/");
-        FILE_SAVE_DIRECTORY_PATH = SERVLET_CONTEXT_ABSOLUTE_PATH + FILE_STORAGE_DIRECTORY;
-    }
+    @Value("${FILE_UPLOAD_PATH}")
+    private String FILE_STORAGE_DIRECTORY;
 
     @RequestMapping
     public CommonJsonResponse uplaodFile(MultipartFile file, HttpSession session){
         if(file.isEmpty())
             return new CommonJsonResponse(ResponseCode.FileUpload.FILE_NOT_ATTACHED);
 
-        ensureFileSaveDirectoryExist(FILE_SAVE_DIRECTORY_PATH);
+        Objects.requireNonNull(FILE_STORAGE_DIRECTORY);
+        ensureFileSaveDirectoryExist(FILE_STORAGE_DIRECTORY);
 
         String fileName = getName(file);
 
-        File fileStorePath = new File(FILE_SAVE_DIRECTORY_PATH + fileName);
+        File fileStorePath = new File(FILE_STORAGE_DIRECTORY + fileName);
 
         try {
             file.transferTo(fileStorePath);
@@ -54,12 +48,8 @@ public class FileUploadController {
         return successJsonResponse(fileName);
     }
 
-    private String extractRelativePath(File pathTobeSaved) {
-        return pathTobeSaved.toString().replace(SERVLET_CONTEXT_ABSOLUTE_PATH, "");
-    }
-
     private String getName(MultipartFile file) {
-        return FILE_SAVE_DIRECTORY_PATH + System.currentTimeMillis() + UUID.randomUUID().toString().replace("-", "") + extractFileExtention(file.getOriginalFilename());
+        return System.currentTimeMillis() + UUID.randomUUID().toString().replace("-", "") + extractFileExtention(file.getOriginalFilename());
     }
 
     private void ensureFileSaveDirectoryExist(String fileSaveDirectoryPath) {
