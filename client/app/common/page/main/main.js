8618259c76d890fc3ec2ab2fc44a60cdf128a6e7
dev <parksungho86@gmail.com>
2015-11-05 00:57:01 +0900

    main page freewall adopt

--- client/app/common/page/main/main.js
+++ client/app/common/page/main/main.js
@@ -3,12 +3,35 @@ angular.module('clientApp').config(function ($stateProvider) {
     .state('main', {
       url: "/",
       templateUrl: "/common/page/main/main.html",
-      headerClass:'navbar-fixed-top navbar-transparent',
-      controller: function ($scope, user, contentBroker) {
+      headerClass: 'navbar-fixed-top navbar-transparent',
+      controller: function ($scope, user, contentBroker, types, $timeout) {
         $scope.user = user;
-        contentBroker.getList(function(list){
-          $scope.contents = list;
+        $scope.types = types;
+
+        var wall = new freewall('#freewall');
+        $scope.$watch(function () {
+          wall.fitWidth();
         });
+
+
+        contentBroker.getList(function (list) {
+          $scope.contents = list;
+          list.forEach(function (content) {
+            content.style = ranStyle(content);
+          });
+        });
+
+        function ranStyle(content) {
+          var style = {};
+          style.width = ['200px', '300px', '400px'][ranInt(3)];
+          style.height = ['200px', '300px', '250px'][ranInt(3)];
+          style['background-color'] = ["#149c82", "#e74c3c", "#3498db"][content.type];
+          return style;
+        }
+
+        function ranInt(int) {
+          return parseInt(Math.random() * int);
+        }
 
 
       }
--- client/app/common/service/types.js
+++ client/app/common/service/types.js
@@ -2,6 +2,7 @@ angular.module('clientApp')
   .factory('types', function () {
     return {
       majorTypes: ["전공필수", "전공선택", "교양"],
-      contentTypes :["강의자료", "질문", "과제"]
+      contentTypes: ["강의자료", "질문", "과제"],
+      contentIcons: ['fa-newspaper-o', 'fa-question-circle', 'fa-home']
     };
   });
