e80654e3f59fcfe6ba1c3aebab680f1b689369ff
park <parksungho86@gmail.com>
2015-11-09 10:24:12 +0900

    surfire plugin remove

--- src/test/java/org/next/Test.java
+++ src/test/java/org/next/Test.java
@@ -11,8 +11,8 @@ public class Test {
     @org.junit.Test
     public void testName() throws Exception {
 //        System.out.println(CommonUtils.parseList(Lesson.class, "[{\"start\":\"2015-11-13T00:05:00.000Z\"},{},{}]").get(0));
-        ObjectMapper mapper = new ObjectMapper();
-        List<Long> list = mapper.readValue("", mapper.getTypeFactory().constructCollectionType(List.class, Long.class));
-        System.out.println(list);
+//        ObjectMapper mapper = new ObjectMapper();
+//        List<Long> list = mapper.readValue("", mapper.getTypeFactory().constructCollectionType(List.class, Long.class));
+//        System.out.println(list);
     }
 }
