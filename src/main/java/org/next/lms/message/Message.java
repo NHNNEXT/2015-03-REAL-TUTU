88f14b5b95d3d4b70259f057907c8c564d836b58
park <parksungho86@gmail.com>
2015-11-19 20:07:26 +0900

    enumarated

--- src/main/java/org/next/lms/message/Message.java
+++ src/main/java/org/next/lms/message/Message.java
@@ -29,7 +29,7 @@ public class Message {
     @Column(name = "MESSAGE")
     private String message;
 
-    @Enumerated
+    @Enumerated(EnumType.STRING)
     @Column(name = "TYPE")
     private MessageType type;
 
