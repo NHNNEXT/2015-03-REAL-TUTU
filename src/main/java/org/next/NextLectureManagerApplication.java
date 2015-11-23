8f716189d09484abd1628c3ad1164f852cee2d3f
ho <hataeho1@naver.com>
2015-11-24 01:35:53 +0900

    Removing non-used code

--- src/main/java/org/next/NextLectureManagerApplication.java
+++ src/main/java/org/next/NextLectureManagerApplication.java
@@ -1,20 +1,12 @@
 package org.next;
 
-import org.next.lms.user.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NextLectureManagerApplication {
 
-    @Autowired
-    UserService userService;
-
-
     public static void main(String[] args) {
         SpringApplication.run(NextLectureManagerApplication.class, args);
-
-
     }
 }
