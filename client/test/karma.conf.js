2c7aa8b8c9ea7b5cf3ce317ba5fd3174e04f1ef1
dan <marvel94@nhnnext.org>
2015-11-24 23:19:16 +0900

    youtube

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -43,6 +43,7 @@ module.exports = function(config) {
       'bower_components/angular-material-data-table/dist/md-data-table.min.js',
       'bower_components/moment/moment.js',
       'bower_components/fullcalendar/dist/fullcalendar.js',
+      'bower_components/satellizer/satellizer.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
