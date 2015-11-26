0e4c8abe652d3277b4f81e64ff8c8a6261a7e555
dev <parksungho86@gmail.com>
2015-11-27 07:55:10 +0900

    text distinct

--- src/main/java/org/next/lms/tag/repository/TagRepository.java
+++ src/main/java/org/next/lms/tag/repository/TagRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    List<Tag> findDistinctTextgitByTextContaining(String keyword);
+    List<Tag> findDistinctTextByTextContaining(String keyword);
 
     void deleteByContentId(Long id);
 }
