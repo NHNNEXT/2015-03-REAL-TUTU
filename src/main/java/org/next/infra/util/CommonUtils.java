3c31c6e0b7fdab808dac3426129da4102a1f150a
ho <hataeho1@naver.com>
2015-11-27 20:40:12 +0900

    add static ellipsis method

--- src/main/java/org/next/infra/util/CommonUtils.java
+++ src/main/java/org/next/infra/util/CommonUtils.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.next.infra.exception.WrongAccessException;
 
 import java.io.IOException;
+import java.security.InvalidParameterException;
 import java.util.List;
 
 import static java.lang.Math.toIntExact;
@@ -38,5 +39,26 @@ public class CommonUtils {
      */
     public static int random(int maxValue) {
         return toIntExact(Math.round((Math.random() * maxValue)));
+    }
+
+    /**
+     * 긴 문자열에 대해 말 줄임 처리를 수행한다
+     *
+     * @param text 말 줄임 처리를 할 문자열
+     * @param length 생략되면 안되는 길이
+     * @return 말줄임 처리된 문자열
+     */
+    public static String ellipsis(String text, int length){
+        if(text.length() <= 0 || length <= 0)
+            throw new InvalidParameterException("length 는 0 이하일 수 없습니다");
+
+        String decoString = "...";
+        String reduced = null;
+
+        if(text.length() > length){
+            reduced = text.substring(0, length);
+            reduced += decoString;
+        }
+        return reduced;
     }
 }
