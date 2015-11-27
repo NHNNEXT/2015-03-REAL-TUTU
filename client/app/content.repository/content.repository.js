f68b1d62f68e4d872524185fb1642ec0af770005
dan <marvel94@nhnnext.org>
2015-11-27 12:17:10 +0900

    repo

--- client/app/content.repository/content.repository.js
+++ client/app/content.repository/content.repository.js
@@ -1,37 +1,64 @@
+/**
+ * Created by itmnext13 on 2015. 11. 25..
+ */
 angular.module('clientApp')
   /* @ngInject */
-  .factory('Content', function (http, $sce, $q, $state, confirm) {
-    function Content(param) {
-      if (param === undefined)
-        return;
-      if (typeof param === "object") {
-        this.setProperties(param);
+  .service('ContentRepository', function (Content,http, $sce, $q, $state, confirm, Reply,Restangular) {
+
+    this.findByContentID = findByContentID;
+    this.findByLectureID = findByLectureID;
+    this.postContent = postContent;
+    this.contentList = {};
+
+    function findByContentID(contentID) {
+      var content = contentList[contentID];
+      if(content) {
+        return content;
+      } else {
+        contentList[contentID] =  JSON.parse(Restangular.one('content',contentID).get());
+        return contentList[contentID];
       }
     }
 
-    Content.findById = function (id) {
-      return $q(function (resolve) {
-        http.get('/api/v1/content', {id: id}).then(function (result) {
-          resolve(new Content(result));
-        });
-      });
-    };
+    function ListContainAllLectureContent(LectureID) {
+      _.chain(contentList)
+        .sortBy('lectureId')
+        .matchesProperty('lectureId', LectureID)
+        .foreach()
+
+
+    }
+    function findByLectureID(LectureID) {
+      var ListContainAllLectureContent = false;
+
+
+      return Restangular.one('lecture',LectureID).getList('content');
+    }
+
+    function postContent(content) {
+      return Restangular
+    }
 
     Content.prototype.setProperties = function (obj) {
-      this.submitRequiredUsers = obj.submitRequiredUsers;
+      this.replies = [];
+      if (obj.replies !== undefined && obj.replies.forEach !== undefined) {
+        var self = this;
+        obj.replies.forEach(function (reply) {
+          self.replies.push(new Reply(reply));
+        });
+      }
       this.repliesSize = obj.repliesSize;
       this.writer = obj.writer;
       this.lectureName = obj.lectureName;
       this.lectureId = obj.lectureId;
       this.id = obj.id;
       this.title = obj.title;
-      this.tags = obj.tags === undefined ? [] : obj.tags;
-        this.body = obj.body;
-      if (obj.writeDate)
+      this.body = obj.body;
+      if (obj.writeDate !== undefined)
         this.writeDate = new Date(obj.writeDate);
-      if (obj.startTime)
+      if (obj.startTime !== undefined)
         this.startTime = new Date(obj.startTime);
-      if (obj.endTime)
+      if (obj.endTime !== undefined)
         this.endTime = new Date(obj.endTime);
       this.type = obj.type; //타입오브젝트
       this.hits = obj.hits;
@@ -42,7 +69,7 @@ angular.module('clientApp')
       return $sce.trustAsHtml(this.body);
     };
 
-    Content.prototype.getQuery = function () {
+    Content.prototype.getQuery = function(){
       var query = {};
       query.id = this.id;
       query.title = this.title;
@@ -51,19 +78,8 @@ angular.module('clientApp')
       query.endTime = this.endTime;
       query.startTime = this.startTime;
       if (this.id === undefined)
-        query.type = this.type.id;
-      if (this.type.submit) {
-        query.submitRequiredUsers = [];
-        this.users.forEach(function (user) {
-          if (user.submit)
-            query.submitRequiredUsers.push(user.id);
-        });
-      }
+        query.type = this.type;
       return query;
-    };
-
-    Content.prototype.setType = function (type) {
-      this.type = type;
     };
 
     Content.prototype.save = function () {
