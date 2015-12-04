3c9e5acc5e18cdb470abc504c33a428ad78c7c4a
dev <parksungho86@gmail.com>
2015-12-05 03:54:02 +0900

    submit status directive add

---
+++ client/app/submit.directive.submit-status/submit-status.js
@@ -0,0 +1,62 @@
+angular.module('clientApp')
+  .directive('submitStatus', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        submitRequiredUsers: '=',
+        deadline: '='
+      },
+      templateUrl: '/submit.directive.submit-status/submit-status.html',
+      /* @ngInject */
+      controller: function ($scope) {
+
+        $scope.left = function (date) {
+          var style = {};
+          style.left = calculateLeft(date);
+          style.height = '100%';
+          style.position = 'absolute';
+          return style;
+        };
+
+        var day = 24 * 60 * 60 * 1000;
+        $scope.$watch('deadline', function (deadline) {
+          if (!deadline)
+            return;
+          var dates = $scope.dates = [];
+          for (var i = -5; i < 3; i++) {
+            var date = new Date(deadline);
+            date.setDate(date.getDate() + i);
+            dates.push(date);
+          }
+          var start = $scope.start = new Date(deadline);
+          start.setDate(start.getDate() - 5);
+          start.setHours(0);
+          start.setMinutes(0);
+          start.setMilliseconds(0);
+          $scope.deadlineStyle = {};
+          $scope.deadlineStyle.left = calculateLeft(deadline);
+        });
+
+        $scope.$watch('submitRequiredUsers', function (users) {
+          if (!users)
+            return;
+          users.forEach(function (user) {
+            if (!user.submits || user.submits.length === 0)
+              return;
+            user.submitted = user.submits.find(function (submit) {
+              return submit.writeDate < $scope.deadline;
+            });
+          });
+        });
+
+        function calculateLeft(date) {
+          var left = 10 + (((date - $scope.start) / (day * 9)) * 100);
+          if (left < 10)
+            left = 10;
+          return left + '%';
+        }
+
+
+      }
+    };
+  });
