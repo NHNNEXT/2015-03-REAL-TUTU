cca1ccf7c8075fc728571f8fed8ab21c73dee7c4
park <parksungho86@gmail.com>
2015-12-06 21:07:58 +0900

    content dao debugg

--- src/main/java/org/next/lms/content/dao/ContentDao.java
+++ src/main/java/org/next/lms/content/dao/ContentDao.java
@@ -26,14 +26,16 @@ public class ContentDao {
         QContent qContent = QContent.content;
         JPAQuery query = new JPAQuery(entityManager);
         query = query.from(qContent);
-        Date start = new Date(this.start);
-        Date end = new Date(this.end);
         if (this.lectureId != null)
             query = query.where(qContent.lecture.id.eq(this.lectureId));
         if (this.keyword != null)
             query = query.where(qContent.title.like(getLikeExpression(this.keyword)).or(qContent.body.like(getLikeExpression(this.keyword)))).limit(AppConfig.pageSize);
-        if (this.start != null && this.end != null)
+        if (this.start != null && this.end != null){
+            Date start = new Date(this.start);
+            Date end = new Date(this.end);
             query = query.where(qContent.startTime.between(start, end).or(qContent.endTime.between(start, end).or(qContent.writeDate.between(start, end))));
+        }
+
 
         // [TODO] 리미트 정책 case 1 : 클라이언트에서 리미트를 요청할 경우 -> 리미트를 큰 숫자로 해서 마음대로 요청할 수 있음
         //                 case 2 : 리미트를 강제적으로 모두 걸 경우 -> date로 between 검색시 기간 내에 이벤트가 있음에도 불구하고 limit에 걸려 내려가지 않을 수 있음 : 마감기한을 놓칠 수 있음.
