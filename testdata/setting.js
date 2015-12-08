f952a436380ddbb165cef1b3ebcd1a3e0405442f
dev <parksungho86@gmail.com>
2015-12-09 04:28:29 +0900

    EOL OS 디펜 던시 제거

--- testdata/setting.js
+++ testdata/setting.js
@@ -1,9 +1,10 @@
 var fs = require('fs');
+var eol = require('os').EOL;
 
 var setting = new Promise(function(resolve){
     var map = {};
     fs.readFile("./../src/main/resources/application.properties", "utf8", function (err, data) {
-        data.split("\n").forEach(function (line) {
+        data.split(eol).forEach(function (line) {
             if (line.match("=")) {
                 var split = line.split("=");
                 map[split[0]] = split[1];
