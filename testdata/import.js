14c7752067e9e427f3160fdf54c7a1fd627ddf55
park <parksungho86@gmail.com>
2015-12-08 22:54:17 +0900

    데이터 임포트 시 테이블 리셋 가능

--- testdata/import.js
+++ testdata/import.js
@@ -22,22 +22,42 @@ var tables = [
     "user_likes_reply"
 ];
 
+var reset = process.argv.indexOf("--reset") !== -1;
+
 var setting = require('./setting.js');
 var mysql = require("mysql");
 var con;
 
-setting.then(function(map){
+setting.then(function (map) {
     con = mysql.createConnection({
         host: "localhost",
         database: "NEXT-LectureManager",
         user: map["spring.datasource.username"],
         password: map["spring.datasource.password"]
     });
-    doImport();
+    var tableCopy = copy(tables);
+    if (reset) {
+        doDelete(tableCopy);
+        return;
+    }
+    doImport(copy(tables));
 });
 
 
-function doImport() {
+function doDelete(tableNames) {
+    var tableName = tableNames.pop();
+    con.query(getDeleteSql(tableName), function (err) {
+        if (err) throw err;
+        console.log(tableName + " Delete Success");
+        if (tableNames.length === 0) {
+            doImport(copy(tables));
+            return;
+        }
+        doDelete(tableNames);
+    });
+}
+
+function doImport(tables) {
     var tableName = tables.splice(0, 1);
     con.query(getImportSql(tableName), function (err) {
         if (err) throw err;
@@ -47,8 +67,13 @@ function doImport() {
             process.exit();
             return;
         }
-        doImport();
+        doImport(tables);
     });
+}
+
+
+function getDeleteSql(tableName) {
+    return "delete from " + tableName;
 }
 
 function getImportSql(tableName) {
@@ -57,4 +82,12 @@ function getImportSql(tableName) {
         "\" INTO TABLE " +
         tableName +
         " FIELDS TERMINATED BY \",\";";
+}
+
+function copy(array) {
+    var result = [];
+    array.forEach(function (each) {
+        result.push(each);
+    });
+    return result;
 }
--- testdata/readme.md
+++ testdata/readme.md
@@ -1,6 +1,7 @@
-node export.js
-node export.js --reset
-node import.js
+node export.js // 익스포트함
+node export.js --reset //원래 있던 파일 지우고 익스포트
+node import.js // 임포트
+node import.js --reset // 원래있던 데이터 지우고 임포트
 
 이슈
 1. 노드 4.2버전에서 Mysql 타임아웃 오류남 5.0으로 업글
