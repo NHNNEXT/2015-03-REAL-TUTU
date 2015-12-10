d39a019096d09c5fe515e3a4738868cc752fe167
park <parksungho86@gmail.com>
2015-12-10 15:52:23 +0900

    angular 머터리얼 11.4데이데이트 피커 에러 부분 css 추가

--- client/app/lecture.directive.lecture-summary/lecture-summary.js
+++ client/app/lecture.directive.lecture-summary/lecture-summary.js
@@ -8,26 +8,40 @@ angular.module('clientApp')
       templateUrl: '/lecture.directive.lecture-summary/lecture-summary.html',
       /* @ngInject */
       controller: function (Content, $scope) {
-        Content.getList({
-          lectureId: $scope.lecture.id,
-          start: $scope.start.getTime(),
-          end: $scope.end.getTime()
-        }).then(function (result) {
-          var now = new Date();
-          var day = 24 * 60 * 60 * 1000;
-          $scope.contents = result;
-          $scope.divider = $scope.contents.length;
-          $scope.contents.forEach(function (content) {
-            content.row = 1;
-            content.col = 1;
-            if (Math.abs(content.writeDate - now) < day)
-              content.row++;
-            if (Math.abs(content.startTime - now) < day)
-              content.col++;
-            if (Math.abs(content.endTime - now) < day)
-              content.col++;
+        $scope.$watch('start', update, true);
+        $scope.$watch('end', update, true);
+        var now = new Date();
+        var day = 24 * 60 * 60 * 1000;
+
+        function update() {
+          if (!$scope.start || !$scope.end)
+            return;
+          Content.getList({
+            lectureId: $scope.lecture.id,
+            start: $scope.start.getTime(),
+            end: $scope.end.getTime()
+          }).then(function (result) {
+            $scope.divider = 3;
+            $scope.contents = result;
+            $scope.divider = $scope.contents.length;
+            $scope.contents.forEach(function (content) {
+              content.row = 1;
+              content.col = 1;
+              if (Math.abs(content.writeDate - now) < day)
+                content.row++;
+              if (Math.abs(content.startTime - now) < day)
+                content.col++;
+              if (Math.abs(content.endTime - now) < day)
+                content.col++;
+            });
           });
-        });
+
+          $scope.dates = [];
+          $scope.dates.push(new Date($scope.start.getTime() + (($scope.end - $scope.start) / 100 * 25)));
+          $scope.dates.push(new Date($scope.start.getTime() + (($scope.end - $scope.start) / 100 * 50)));
+          $scope.dates.push(new Date($scope.start.getTime() + (($scope.end - $scope.start) / 100 * 75)));
+          $scope.now = {left: calculateLeft(new Date())};
+        }
 
         $scope.class = function (content) {
           var classes = {SUBMIT: 'green', GENERAL: 'yellow', SCHEDULE: 'blue', NOTICE: 'purple'};
@@ -37,9 +51,9 @@ angular.module('clientApp')
           return classes[content.contentGroup.contentType] + ' hover';
         };
 
-
-
-        $scope.divider = 3;
+        $scope.nowCovered = function () {
+          return $scope.start < now && now < $scope.end;
+        };
 
         $scope.left = function (date, index) {
           var style = {};
@@ -58,6 +72,7 @@ angular.module('clientApp')
           style.top = (100 / $scope.divider) * (index % $scope.divider) + '%';
           return style;
         };
+
 
         function calculateLeft(date) {
           var left = (((date - $scope.start) / ($scope.end - $scope.start)) * 100);
