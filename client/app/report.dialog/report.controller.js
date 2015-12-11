a0c7ef55fd9cfa1149de53197bddf7a9496d3aa7
dev <parksungho86@gmail.com>
2015-12-11 13:27:48 +0900

    리포트 후 대화상자 클로즈

--- client/app/report.dialog/report.controller.js
+++ client/app/report.dialog/report.controller.js
@@ -1,6 +1,6 @@
 angular.module('clientApp')
   /* @ngInject */
-  .controller('reportController', function ($scope, rootUser, $state, http, alert) {
+  .controller('reportController', function ($scope, rootUser, $state, http, alert, dialog) {
     $scope.rootUser = rootUser;
     $scope.$state = $state.current;
     $scope.send = function () {
@@ -11,6 +11,7 @@ angular.module('clientApp')
       body.body = $scope.body;
       http.post('/api/v1/report', {body: JSON.stringify(body)}).then(function(){
         alert.success("감사합니다.");
+        dialog.close();
       });
     };
   });
