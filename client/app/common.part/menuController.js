d7b7b2aa28c9c88685e4419688e09150c0fd0eaf
dev <parksungho86@gmail.com>
2015-12-14 03:10:02 +0900

    2차 데모

--- client/app/common.part/menuController.js
+++ client/app/common.part/menuController.js
@@ -1,40 +1,10 @@
 angular.module('clientApp')
   .controller('menuController',
     /* @ngInject */
-    function ($scope, rootUser,$mdSidenav,$state, Message, mainButler) {
+    function ($scope, rootUser, $mdSidenav, $state, Message) {
 
       $scope.rootUser = rootUser;
       $scope.Message = Message;
-
-      var vm = $scope;
-      vm.date = mainButler.date;
-
-      vm.setNowAndClose = function() {
-        vm.setDateNow();
-        vm.close();
-      };
-      vm.setDateNow = function() {
-        mainButler.setDateNow();
-        vm.date = mainButler.date;
-        if($state.is("main")) {
-          $state.reload("main");
-        } else {
-          $state.go("main");
-        }
-        return vm;
-      };
-
-      vm.setDateOneWeek = function() {
-        mainButler.setDateOneWeek();
-        vm.date = mainButler.date;
-        if($state.is("main")) {
-          $state.reload("main");
-        }else {
-          $state.go("main");
-        }
-
-        return vm;
-      };
       $scope.close = function () {
         $mdSidenav('left').close();
       };
