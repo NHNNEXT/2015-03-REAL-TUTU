fba4377e14224511aaef5a9f0fae09c7a61f9562
ho <hataeho1@naver.com>
2015-12-16 02:42:02 +0900

    asynchronous email transmission

--- src/main/java/org/next/infra/mail/MailService.java
+++ src/main/java/org/next/infra/mail/MailService.java
@@ -3,6 +3,7 @@ package org.next.infra.mail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +16,7 @@ public class MailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Async
     public void sendMail(Mail mail) {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message);
