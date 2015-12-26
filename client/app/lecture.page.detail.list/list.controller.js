8f09497f4b1bba80c6e0d9d22fae0138a73a935e
dev <parksungho86@gmail.com>
2015-12-26 16:29:29 +0900

    마이너 디버그

--- client/app/lecture.page.detail.list/list.controller.js
+++ client/app/lecture.page.detail.list/list.controller.js
@@ -1,6 +1,6 @@
 angular.module('clientApp')
   /* @ngInject */
-  .controller('contentListController', function ($scope, Content, $stateParams, $state) {
+  .controller('contentListController', function ($scope, Content, $stateParams) {
     $scope.query = {contentGroupName: $stateParams.contentGroupName, lectureId:$stateParams.lectureId};
 
     $scope.page = 0;
