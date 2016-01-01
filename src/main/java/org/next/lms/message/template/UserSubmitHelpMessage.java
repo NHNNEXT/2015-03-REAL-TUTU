4c73a7201047c7ea7c0083df56ebc3ef56e2b557
dev <parksungho86@gmail.com>
2016-01-01 13:26:40 +0900

    message refactoring

--- src/main/java/org/next/lms/message/template/UserSubmitHelpMessage.java
+++ src/main/java/org/next/lms/message/template/UserSubmitHelpMessage.java
@@ -11,7 +11,7 @@ import static org.next.infra.util.HanGuelUtil.josaWhenEllipsisApplied;
 
 public class UserSubmitHelpMessage extends ImmutableMessageTemplate {
 
-    private static final String messageTemplate = "%s님이 '%s' %s에 도움말을 달았습니다.";
+    private static final String messageTemplate = "%s님이 '%s' %s에 댓글을 달았습니다.";
 
     private static final String singleEventUrlTemplate = "/게시물/%d";
 
@@ -25,7 +25,7 @@ public class UserSubmitHelpMessage extends ImmutableMessageTemplate {
 
     @Override
     protected String getMessageString() {
-        return String.format(messageTemplate, writer.getName(), CommonUtils.ellipsis(content.getTitle(), 30), josaWhenEllipsisApplied(content.getTitle(), HanGuelUtil.JosaType.을를, 30));
+        return String.format(messageTemplate, writer.getName(), CommonUtils.ellipsis(content.getTitle(), 30), content.getTitle());
     }
 
     @Override
