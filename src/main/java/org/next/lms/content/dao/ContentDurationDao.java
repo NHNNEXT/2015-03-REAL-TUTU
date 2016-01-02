42cc009ccdd1205a151bc981f7dc61ccac45a68a
dev <parksungho86@gmail.com>
2016-01-02 11:45:10 +0900

    serverside resolve #72

--- src/main/java/org/next/lms/content/dao/ContentDurationDao.java
+++ src/main/java/org/next/lms/content/dao/ContentDurationDao.java
@@ -15,6 +15,8 @@ public class ContentDurationDao extends ContentDao {
     private Long start;
     private Long end;
 
+    private static final Long MAX_DURATION = 15552000000L; // 1000 * 60 * 60 * 24 * 180 -> 180일
+
     @Override
     protected JPAQuery limitPolicy(JPAQuery query) {
         QContent qContent = QContent.content;
@@ -22,6 +24,8 @@ public class ContentDurationDao extends ContentDao {
             throw new WrongAccessException();
         Date start = new Date(this.start);
         Date end = new Date(this.end);
+        if (end.getTime() - start.getTime() > MAX_DURATION)
+            throw new WrongAccessException();
         return query.where(
                 qContent.startTime.between(start, end).or(qContent.endTime.between(start, end))
                         .or(qContent.startTime.before(start).and(qContent.endTime.after(end)))
