cba3623c4b25651f41e102517978e7e462c477af
park <parksungho86@gmail.com>
2016-01-05 19:31:28 +0900

    읽을 수 없을경우 목록에 읽을수 없다는 정보 표시. 및 클릭해도 이동하지 않음.

--- client/app/content.directive.content-header/content-header.js
+++ client/app/content.directive.content-header/content-header.js
@@ -9,6 +9,16 @@ angular.module('clientApp')
       templateUrl: '/content.directive.content-header/content-header.html',
       link: function (s) {
         s.now = Now;
+      }, controller: function (rootUser, $scope, $state, alert) {
+        $scope.rootUser = rootUser;
+        $scope.readable = rootUser.lectures.findById($scope.content.lectureId).contentGroups.findById($scope.content.contentGroup.id) !== undefined;
+        $scope.ifReadableMove = function () {
+          if (!$scope.readable) {
+            alert.warning("열람 권한이 없습니다.");
+            return;
+          }
+          $state.go('content', {id: $scope.content.id});
+        };
       }
     };
   });
