9aee05407ac9c2166f3fbf01cb9a3472d8ccaada
dev <parksungho86@gmail.com>
2016-01-06 18:32:00 +0900

    dont reset tables

--- src/main/java/org/next/config/InsertTestDataFromCSV.java
+++ src/main/java/org/next/config/InsertTestDataFromCSV.java
@@ -1,18 +1,18 @@
-package org.next.config;
-
-import org.next.dbdump.ImportCSV;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.sql.SQLException;
-
-@Service
-public class InsertTestDataFromCSV {
-    
-    @PostConstruct
-    public void insertTestData() throws IOException, SQLException, ClassNotFoundException {
-        ImportCSV importCSV = new ImportCSV(true);
-        importCSV.exe();
-    }
-}
+//package org.next.config;
+//
+//import org.next.dbdump.ImportCSV;
+//import org.springframework.stereotype.Service;
+//
+//import javax.annotation.PostConstruct;
+//import java.io.IOException;
+//import java.sql.SQLException;
+//
+//@Service
+//public class InsertTestDataFromCSV {
+//
+//    @PostConstruct
+//    public void insertTestData() throws IOException, SQLException, ClassNotFoundException {
+//        ImportCSV importCSV = new ImportCSV(false);
+//        importCSV.exe();
+//    }
+//}
