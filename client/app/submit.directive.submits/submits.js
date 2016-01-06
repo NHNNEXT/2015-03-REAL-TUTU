be4a783e6108b3481cc27e7cd97e8faf8f3f8329
park <parksungho86@gmail.com>
2016-01-06 12:38:06 +0900

    게시물 안에서만 제출 완료여부 선택가능. 제출이 없을떄 경고표시.

--- client/app/submit.directive.submits/submits.js
+++ client/app/submit.directive.submits/submits.js
@@ -7,7 +7,7 @@ angular.module('clientApp')
       },
       templateUrl: '/submit.directive.submits/submits.html',
       /* @ngInject */
-      controller: function (rootUser, $scope, Submit, User, emoticon, http, Page) {
+      controller: function (rootUser, $scope, Submit, User, emoticon, http, Page, alert, confirm) {
 
         $scope.more = function () {
           var query = {};
@@ -25,7 +25,6 @@ angular.module('clientApp')
         $scope.$watch('submitRequire', function (submitRequire) {
           if (!submitRequire)
             return;
-          $scope.done = submitRequire.done;
           $scope.user = new User(submitRequire.user);
           $scope.submitId = submitRequire.id;
           $scope.more();
@@ -36,9 +35,30 @@ angular.module('clientApp')
         var page = $scope.page = new Page();
 
 
-        $scope.doToggle = function () {
-          http.post('/api/v1/content/submit/require', {id: $scope.submitId, done: !$scope.done}); //NG-Model바뀌는거보다, 이벤트 실행이 먼저되서 !done으로 보내야함.
+        $scope.doToggle = function (ev) {
+          var state = !$scope.submitRequire.done;//NG-Model바뀌는거보다, 이벤트 실행이 먼저되서 !done으로 보내야함.
+
+          if (state && $scope.submits.length === 0) {
+            confirm("제출한 내역이 없습니다. 그래도 제출 완료상태로 바꾸시겠습니까?", "", function () {
+              postToggle(state);
+            }, function () {
+              $scope.submitRequire.done = false;
+            });
+            return;
+          }
+          postToggle(state);
         };
+
+        function postToggle(state) {
+          http.post('/api/v1/content/submit/require', {id: $scope.submitId, done: state}).then(function () {
+            if (state) {
+              alert.info("완료 되었습니다. 제출 목록에 표시되지 않습니다.");
+              return;
+            }
+            alert.info("제출 목록에 표시합니다.");
+          });
+        }
+
 
         $scope.submit = new Submit();
         $scope.rootUser = rootUser;
